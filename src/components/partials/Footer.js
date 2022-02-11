4203911e22e6ca1854fd12d2e256d20e13a7db72
Uma Shankar <umashankarsh1@yahoo.com>
2022-02-11 22:48:46 +0530

    added bootstrap button in footer

--- src/components/partials/Footer.js
+++ src/components/partials/Footer.js
@@ -1,14 +1,21 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Row, Col, Button } from 'react-bootstrap';
 import "./Footer.css";
 
 class Footer extends Component {
     render() {
-        return (
+        return (<>
+            <Row className="mx-0">
+                <Button as={Col} variant="primary">Button #1</Button>
+                <Button as={Col} variant="secondary" className="mx-2">Button #2</Button>
+                <Button as={Col} variant="success">Button #3</Button>
+            </Row>
             <footer className="footer mt-auto py-3 bg-light">
                 <div className="container">
                     <span className="text-muted">Place sticky footer content here.</span>
                 </div>
             </footer>
+        </>
         );
     }
 }
